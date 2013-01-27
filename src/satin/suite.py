6674ff376658c90338cc72ba2c3bf7916e5b1f45
Tuukka Turto <tuukka.turto@oktaeder.net>
2013-01-27 23:20:22 +0200

    clean up

--- src/satin/suite.py
+++ src/satin/suite.py
@@ -18,25 +18,16 @@
 #   You should have received a copy of the GNU General Public License
 #   along with satin-python.  If not, see <http://www.gnu.org/licenses/>.
 
-import types
-import threading
 from PyQt4.QtGui import QApplication
 from PyQt4 import QtCore
-from PyQt4.QtCore import SIGNAL, SLOT
 
 def satin_suite(cls):
 
     if hasattr(cls, 'setup'):
-        cls._setup = cls.setup
-        cls.setup = _setup
-        cls.setup.im_func.__name__ = 'setup'
-        cls._setup.im_func.__name__ = '_setup'
+        cls.setup = setup(cls.setup)
 
     if hasattr(cls, 'teardown'):
-        cls._teardown = cls.teardown
-        cls.teardown = _teardown
-        cls.teardown.im_func.__name__ = 'teardown'
-        cls._teardown.im_func.__name__ = '_teardown'
+        cls.teardown = teardown(cls.teardown)
 
     for key, attribute in cls.__dict__.items():
         if hasattr(attribute, '__call__') and 'test_' in key:
@@ -49,13 +40,17 @@ def satin_suite(cls):
 
     return cls
 
-def _setup(self):
-    self.qt_app = QApplication([])
-    self._setup()
+def setup(fn):
+    def setup(self):
+        self.qt_app = QApplication([])
+        fn(self)
+    return setup
 
-def _teardown(self):
-    self.qt_app = None
-    self._teardown()
+def teardown(fn):
+    def teardown(self):
+        self.qt_app = None
+        fn(self)
+    return teardown
 
 def get_wrapper(original_step):
     def _wrapper(self):
@@ -69,9 +64,8 @@ def get_test_step(original_step):
         timer.setSingleShot(True)
         timer.start()
 
-        QtCore.QObject.connect(timer,
-                           QtCore.SIGNAL("timeout()"),
-                           getattr(self, '_wrapper_{0}'.format(original_step)))
+        timer.timeout.connect(getattr(self,
+                                '_wrapper_{0}'.format(original_step)))
 
         self.qt_app.exec_()
     return _step
